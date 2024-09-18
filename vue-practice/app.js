8c747bd66d6d040d9095c1558810d5466809ad16
Eve-GLH <56955780+Eve-GLH@users.noreply.github.com>
2024-09-18 16:26:37 +0100

    corrected things, made it dark mode

--- vue-practice/app.js
+++ vue-practice/app.js
@@ -2,6 +2,6 @@ const app = new Vue({
     el: '#app',
     data: {
         noteTitle: 'Here is the title of the note',
-        noteBody: "This is the body of the note. It/'s going to be an awful lot of text but that's sort of the point, just to make sure that it will all pull through and all show on the page and such."
+        noteBody: 'This is the body of the note. It\'s going to be an awful lot of text but that\'s sort of the point, just to make sure that it will all pull through and all show on the page and such.'
     }
 });
