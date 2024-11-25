be09ffa4b205685ca3476a043471bb8e9ab802ad
Eve-GLH <56955780+Eve-GLH@users.noreply.github.com>
2024-11-25 17:49:18 +0000

    made a table

--- times-tables/app.js
+++ times-tables/app.js
@@ -1,9 +1,28 @@
 const app = new Vue ({
     el: '#app',
     data: {
-
+        num1: 5,
+        num2: 5
+    },
+    computed: {
+        multipliers() {
+            const results = [];
+            for (let i=1; i <= this.num2; i++){
+                results.push({
+                    m: i,
+                    total: this.num1 * i
+                });
+            }
+            return results;
+        }
     },
     methods: {
-        
+        createTable: function() {
+            this.multipliers = [];
+            
+        }
+    },
+    mounted() {
+        this.createTable();
     }
 })
