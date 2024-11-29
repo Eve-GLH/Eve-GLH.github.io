67e53e3b912d0d24930b0e00d49ae0d13e3f276f
Eve-GLH <56955780+Eve-GLH@users.noreply.github.com>
2024-11-29 13:12:00 +0000

    validation

--- times-tables/app.js
+++ times-tables/app.js
@@ -2,10 +2,14 @@ const app = new Vue ({
     el: '#app',
     data: {
         num1: 5,
-        num2: 5
+        num2: 5,
+        warningClass: 'warning',
+        validClass: 'valid',
+        isValid: true
     },
     computed: {
         multipliers() {
+            if (!this.isValid) return;
             const results = [];
             for (let i=1; i <= this.num2; i++){
                 results.push({
@@ -17,9 +21,20 @@ const app = new Vue ({
         }
     },
     methods: {
-        createTable: function() {
-            this.multipliers = [];
-            
+        isValidFunc: function() {
+            if (this.num1 >= 1 && this.num1 <= 12 && this.num2 >= 1 && this.num2 <= 12) {
+                this.isValid = true;
+            } else {
+                this.isValid = false;
+            }
+        }
+    },
+    watch: {
+        num1(value) {
+            this.isValidFunc();
+        },
+        num2(value) {
+            this.isValidFunc();
         }
     },
     mounted() {
