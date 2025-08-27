fd2714c19be4f263e675ddce0c9f3168ba8a2116
Eve-GLH <56955780+Eve-GLH@users.noreply.github.com>
2025-08-27 22:23:46 +0100

    bloom and balance yoga site added

---
+++ portfolio/bloom-and-balance-yoga/bloom-and-balance.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+  $('.testimonials__inner').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    cssEase: 'linear',
+    arrows: false,
+    dots: true
+  });
+});
