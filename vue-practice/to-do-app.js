ba49ce2185e9d500cc1a53e3ecffa4e659544be4
Eve-GLH <56955780+Eve-GLH@users.noreply.github.com>
2026-07-23 17:10:28 +0100

    Editable title!

--- vue-practice/to-do-app.js
+++ vue-practice/to-do-app.js
@@ -1,14 +1,30 @@
 const ToDoApp = new Vue({
     el: '#ToDoApp',
     data: {
-        title: 'My To Do List'
+        title: 'My To Do List',
+        inputTitle: ''
     },
-    conputed: {
+    computed: {
 
     },
     methods: {
         resetFields: function() {
             this.title = 'My To Do List'
+        },
+        showEdit: function() {
+            let titleInput = document.getElementById("TitleEdit");
+            if (titleInput.style.display === "none") {
+                titleInput.style.display = "block";
+            } else {
+                titleInput.style.display = "none";
+            }
+        },
+        submitTitle: function() {
+            if (this.inputTitle.length < 3) {
+                this.title = "My To Do List"
+            } else {
+                this.title = this.inputTitle;
+            }
         }
     }
 });
