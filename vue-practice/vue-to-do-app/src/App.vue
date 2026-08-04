d26aa0a4d78117a35fd0f28d2834d9754ff083d3
Eve-GLH <56955780+Eve-GLH@users.noreply.github.com>
2026-08-04 15:34:41 +0100

    slightly styled

--- vue-practice/vue-to-do-app/src/App.vue
+++ vue-practice/vue-to-do-app/src/App.vue
@@ -32,65 +32,71 @@ const toggleDone = (item) => {
 }
 </script>
 <template>
-  <header class="header">
-    <h1>{{ title.toLocaleUpperCase() }}</h1>
-    <button v-if="editing" class="btn" @click="doEdit(false)">
-      Cancel
-    </button>
-    <button v-else class="btn btn-primary" @click="doEdit(true)">
-      Add Item
-    </button>
-  </header>
-  <form
-    class="add-item-form"
-    v-if="editing"
-    @submit.prevent="saveItem"
-  >
+  <div class="body">
     <div class="container">
-      <input v-model.trim="newItem" type="text" placeholder="Add an item">
-      <br />
-      <br />
-      Priority:
-      <select v-model="newItemPriority">
-        <option value="low">Low</option>
-        <option value="medium">Medium</option>
-        <option value="high">High</option>
-      </select>
-
-      <label>
-        <input type="checkbox" v-model="newItemDone">
-        Done?
-      </label>
-      
-      <button :disabled="newItem.length < 3" class="btn btn-primary">
-        Save Item
-      </button>
-    </div>
-  </form>
-
-  <div class="output-section">
-    <div class="container">
-      <ul>
-        <li
-        v-for="(item, index) in reversedItems"
-        @click="toggleDone(item)"
-        :key="item.id"
-        class="list-item"
-        :class="{
-          strikeout: item.done,
-          priorityH: item.priority === 'high',
-          priorityM: item.priority === 'medium'
-        }"
+      <div class="body-inner">
+        <header class="header">
+          <h1>{{ title.toLocaleUpperCase() }}</h1>
+          <button v-if="editing" class="btn" @click="doEdit(false)">
+            Cancel
+          </button>
+          <button v-else class="btn btn-primary" @click="doEdit(true)">
+            Add Item
+          </button>
+        </header>
+        <form
+          class="add-item-form"
+          v-if="editing"
+          @submit.prevent="saveItem"
         >
-          {{ item.label }}
-        </li>
-      </ul>
-      <p v-if="!items.length">
-        Nothing to see here...
-      </p>
+          <div class="form-inner">
+            <input class="item-input" v-model.trim="newItem" type="text" placeholder="Add an item">
+            <div class="priority-boxes">
+              Priority:
+              <select v-model="newItemPriority">
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+              </select>
+            </div>
+            <div class="done-boxes">
+              <label>
+                <input type="checkbox" v-model="newItemDone">
+                Done?
+              </label>
+            </div>
+            
+            <button :disabled="newItem.length < 3" class="save-btn btn btn-primary">
+              Save Item
+            </button>
+          </div>
+        </form>
+
+        <div class="output-section">
+          <div class="container">
+            <ul>
+              <li
+              v-for="(item, index) in reversedItems"
+              @click="toggleDone(item)"
+              :key="item.id"
+              class="list-item"
+              :class="{
+                strikeout: item.done,
+                priorityH: item.priority === 'high',
+                priorityM: item.priority === 'medium'
+              }"
+              >
+                {{ item.label }}
+              </li>
+            </ul>
+            <p v-if="!items.length">
+              Nothing to see here...
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-    
 </template>
 
 <style scoped>
@@ -106,5 +112,42 @@ const toggleDone = (item) => {
   }
   .priorityH {
     color: red;
+  }
+  .body-inner {
+    border: 1px solid #222;
+    border-radius: 50px;
+    overflow: hidden;
+  }
+
+  .header {
+      background: #222;
+      color: #FFF;
+      padding: 30px 0;
+  }
+  .add-item-form {
+    padding: 60px 30px;
+  }
+  .output-section {
+      padding: 30px 15px;
+  }
+  .form-inner {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 75%;
+    margin: auto;
+  }
+  .priority-boxes {
+    width: 32%;
+  }
+  .done-boxes {
+    width: 32%;
+  }
+  .item-input {
+    width: 100%;
+    margin: 10px auto 20px;
+  }
+  .save-btn {
+    width: 32%;
   }
 </style>
