68cadd470f53dcdab057bdbafe53c769eb59a8ad
Eve-GLH <56955780+Eve-GLH@users.noreply.github.com>
2026-08-10 17:30:17 +0100

    giving data to an api

---
+++ vue-practice/api-user-info/src/App.vue
@@ -0,0 +1,55 @@
+<script>
+  import axios from "axios"
+  export default {
+    data() {
+      return {
+        userId: 1,
+        userInfo: null,
+        error: null
+      }
+    },
+    methods: {
+      getUser() {
+        axios.get(`https://jsonplaceholder.typicode.com/users/${this.userId}`)
+        .then((response) => {
+          this.userInfo = response.data
+        })
+        .catch((err) => {
+          this.error = err.message
+        });
+      }
+    },
+    mounted() {
+    }
+  }
+</script>
+
+<template>
+  <section>
+    <div class="container">
+      <div class="user-change">
+        <label>
+          User ID:
+          <input v-model="this.userId" type="number">
+        </label>
+        <button @click="getUser" class="btn btn-success">Enter User ID</button>
+      </div>
+      <div v-if="userInfo" class="section-inner">
+        <div class="user-name">
+          Name: 
+          {{ userInfo.name }}
+        </div>
+        <div class="user-email">
+          Email:
+          {{ userInfo.email }}
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.section-inner {
+  padding: 60px 0;
+}
+</style>
